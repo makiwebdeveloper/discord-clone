216600eae8b7db2cce20b98bcc9d0c6f29e31c6d
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-08-27 19:21:30 +0200

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+
+const openSans = Open_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Discord",
+  description:
+    "Discord is the easiest way to talk over voice, video, and text.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={openSans.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-semibold">Home</h1>
+    </main>
+  );
+}
